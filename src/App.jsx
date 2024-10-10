e630b4ca8c4fe60ebb2393d6e2d4d8ff6fee14e9
coccalis <kokkalisxristos@gmail.com>
2024-10-10 23:25:22 +0300

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import RootLayout from "./pages/Root";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [{ path: "/", element: <Home /> }],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [{ path: "/", element: <Home /> }],
+    },
+  ],
+  { basename: "/Portfolio" }
+);
 function App() {
   return <RouterProvider router={router} />;
 }
